604fc1636a31593eed311a084ef17e43a62355b6
kidabg548 <kidabg548@gmail.com>
2025-01-10 23:57:39 +0300

    removing some files that are not needed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -19,9 +19,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
         <SearchContextProvider>
-    <App />
-    </SearchContextProvider>
-    </AppContextProvider>
+          <App />
+        </SearchContextProvider>
+      </AppContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
